eef438f36b0d46619cdd33f93e4bc8015ed9dcff
dill337 <64544519+dill337@users.noreply.github.com>
2020-09-21 17:03:24 -0500

    Uploaded files to github

---
+++ index.js
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>Home Page</title>
+<body>
+  <p>
+    The home page of our movie center
+  </p>
+</body>
+</html>
---
+++ test.js
@@ -0,0 +1,8 @@
+console.log('hello node!');
+
+console.log('goodbye');
+console.log('goodbye');
+
+const os = require('os');
+
+console.log('type: ' + os.type());
