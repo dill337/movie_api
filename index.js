4c0db33d22c05f8235fc3789f1d2429b2be207fa
dill337 <64544519+dill337@users.noreply.github.com>
2020-10-03 14:07:40 -0500

    Updated Portal

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const app = express();
 app.use(bodyParser());
 let auth = require('./auth')(app);
 const passport = require('passport');
-require('./passport');
+  require('./passport');
+const cors = require('cors');
+  app.use(cors());
+const { check, validationResult } = require('express-validator');
 
 const Movies = Models.Movie;
 const Users = Models.User;
@@ -37,16 +40,34 @@ app.use((err, req, res, next) => {
   Email: String,
   Birthday: Date,
 }*/
-app.post('/users', (req, res) => {
-  Users.findOne({ Username: req.body.Username })
+app.post('/users',
+  // Validation logic here for requests
+  //Can use chain of methods like .not().isEmpty()- OPPPOsite of isEmpty
+  //Can use .isLength({min: 5}) means minnium of 5 characters only allowed
+  [
+    check('Username', 'Username is required').isLength({min:5}),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], (req, res) => {
+    //check validation object for errors
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+  let hashedPassword = Users.hashedPassword(req.body.Password);
+  Users.findOne({ Username: req.body.Username }) //Search to see if username already exists
     .then((user) =>{
       if (user) {
+        //If user is found, send message that it already exists
         return res.status(400).send(req.body.Username + ' already exists');
       } else {
         Users
           .create({
             Username: req.body.Username,
-            Password: req.body.Password,
+            Password: hashedPassword,
             Email: req.body.Email,
             Birthday: req.body.Birthday
           })
@@ -266,6 +287,7 @@ app.delete('/movies/:title', (req, res) => {
 
 
 // listen for requests
-app.listen(8080, () =>
-  console.log('Your app is listening on port 8080.')
-);
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on Port ' + port);
+});
--- passport.js
+++ passport.js
@@ -20,7 +20,12 @@ passport.use(new LocalStrategy({
 
     if(!user) {
       console.log('incorrect username');
-      return callback(null, false, {message: 'Incorect username or password.'});
+      return callback(null, false, {message: 'Incorect username.'});
+    }
+
+    if (!user.validatePassword(password)) {
+      console.log('incorrect password');
+      return callback(null, false, {message: 'Incorrect Password.'});
     }
     console.log('finished');
     return callback(null, user);
